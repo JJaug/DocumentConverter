ffd9a1111cd28a3087ec15a05a2df07a735e152f
JJaugelis <jonas.jaugas@gmail.com>
2021-08-02 11:32:15 +0300

    Add project files.

---
+++ DocumentConverter.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DocumentConverter.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
